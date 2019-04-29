50d74aa1ddcfc15eabfcb701c2dc75e47c414e95
Christine Cua <cloudycua@gmail.com>
2019-04-29 16:05:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,11 @@ class Song < ActiveRecord::Base
     scope: %i[release_year artist_name]
   }
 
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+
 end
